2644a36b8b7e06cb3e54b6545e41f7e0171c6cb6
Ziping Sun <me@szp.io>
2025-06-03 21:46:41 +0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "virtual environments";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  inputs.systems.url = "github:nix-systems/default";
+
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.devshell.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.devshell.inputs.systems.follows = "systems";
+
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+  outputs =
+    inputs@{
+      flake-parts,
+      devshell,
+      systems,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ devshell.flakeModule ];
+
+      systems = import systems;
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devshells.default = {
+            devshell.packages = with pkgs; [
+              typst
+              typstyle
+
+            ];
+            env = [
+              {
+                name = "FONTCONFIG_FILE";
+                value = pkgs.makeFontsConf {
+                  fontDirectories = with pkgs; [
+                    libertine
+                    source-han-sans
+                  ];
+                };
+              }
+            ];
+          };
+        };
+    };
+}
---
+++ main.typ
@@ -0,0 +1,46 @@
+#import "@preview/touying:0.6.1": *
+#import "@preview/touying-buaa:0.2.0": *
+
+// Specify `lang` and `font` for the theme if needed.
+#show: buaa-theme.with(
+  lang: "zh",
+  font: (
+    (
+      name: "Liberation Sans",
+      covers: "latin-in-cjk",
+    ),
+    "Source Han Sans SC",
+    "Source Han Sans",
+  ),
+  config-info(
+    title: [Touying for BUAA: Customize Your Slide Title Here],
+    subtitle: [Customize Your Slide Subtitle Here],
+    author: [Authors],
+    date: datetime.today(),
+    institution: [Beihang University],
+  ),
+)
+
+#title-slide()
+
+#outline-slide()
+
+= The section I
+
+== Slide I / i
+
+Slide content.
+
+== Slide I / ii
+
+Slide content.
+
+= The section II
+
+== Slide II / i
+
+Slide content.
+
+== Slide II / ii
+
+Slide content.
